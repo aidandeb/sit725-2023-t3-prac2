fb44f919ddcd17b02774c614f21a40a594d0bd91
aidandeb <132536077+aidandeb@users.noreply.github.com>
2023-11-06 12:33:16 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-function changeText(){
-    var textsArray = ["Text 1","Text 2", "Text 3", "Text 4", "Text 5"]
-    var number = getRandomNumberBetween(0,textsArray.length - 1)
-    console.log("Index: ", number)
-    document.getElementById("heading").innerHTML = textsArray[number];
-}
+function performCalculation() {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
 
-function getRandomNumberBetween(min,max){
-    return Math.floor(Math.random()*(max-min+1)+min);
+    const result = num1 + num2;
+
+    document.getElementById("result").innerHTML = `Result: ${result}`;
 }
